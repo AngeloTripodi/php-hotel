a1eb86d34942c2d8d8518dbf025505e8bed78cfd
AngeloTripodi <tripodi.angelo@gmail.com>
2023-01-25 17:11:27 +0100

    Stamp element on page

--- index.php
+++ index.php
@@ -10,6 +10,61 @@
 
 <body>
 
+    <main>
+
+
+        <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+
+        foreach ($hotels as $hotel) {
+            echo "{$hotel['name']} , {$hotel['description']} , {$hotel['parking']}, {$hotel['vote']} , {$hotel['distance_to_center']} </br>";
+        }
+        // echo $hotel['name'];
+        // echo $hotel['description'];
+        ?>
+
+    </main>
+
+
 </body>
 
 </html>
